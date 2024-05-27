a2d9b54bdeee247a3d3b1cfa2eec0a07d328fac1
kroszczak <k_roszczak@icloud.com>
2024-05-27 22:22:37 +0200

    zupa

--- src/WelcomeScreen.js
+++ src/WelcomeScreen.js
@@ -4,10 +4,10 @@ const WelcomeScreen = ({ onStart }) => {
     return (
         <div style={styles.container}>
             <h1>Przedstawiamy: nasze kule (The our balls expreriment)</h1>
-            <p> 1. Upewnij się, że jesteś sam w pomieszczeniu w cichym pomieszczeniu.
-                2. Jeśli możesz, załóż słuchawki
-                3. Ustaw głośność swojego urządzenia na maksymalny poziom.
-
+            <p style={{ maxWidth: 80 + 'vw', fontSize: 1.9 + 'em' }}> 1. Upewnij się, że jesteś sam w pomieszczeniu w cichym pomieszczeniu. <br />
+                2. Jeśli możesz, załóż słuchawki <br />
+                3. Ustaw głośność swojego urządzenia na maksymalny poziom. <br />
+                <br />
                 Zobaczysz kilkanaście kulek odbijających się od ścian. Twoim zadaniem jest policzenie, ile razy czerwone kulki łącznie zderzą się ze ścianą, lub inną kulką.</p>
             <button onClick={onStart} style={styles.button}>Start</button>
         </div>
