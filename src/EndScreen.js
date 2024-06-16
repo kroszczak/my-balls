8edbf273428a611586bcc56616d9bcae9adb458b
kroszczak <k_roszczak@icloud.com>
2024-06-16 19:28:41 +0200

    link hotfix

--- src/EndScreen.js
+++ src/EndScreen.js
@@ -6,7 +6,7 @@ const EndScreen = () => {
             <h1 style={styles.text}>Dziękujemy za udział w badaniu!</h1>
             <br />
 
-            <h2 style={styles.text}>prosimy o wypełnienie ankiety: <br /><a href="http://forms.gle/EMfT3BHJD7sLfbyk7"></a></h2>
+            <h2 style={styles.text}>prosimy o wypełnienie ankiety: <br /><a href="http://forms.gle/EMfT3BHJD7sLfbyk7">http://forms.gle/EMfT3BHJD7sLfbyk7</a></h2>
         </div>
     );
 };
